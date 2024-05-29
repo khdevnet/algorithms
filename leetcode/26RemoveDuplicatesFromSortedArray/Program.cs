772ff0b0cd888be2111b600024f35deaec162991
Anton <khdevnet@gmail.con>
2024-05-29 21:45:10 +0200

    Add code

---
+++ leetcode/26RemoveDuplicatesFromSortedArray/Program.cs
@@ -0,0 +1,38 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // int[] nums = { 1, 2, 3 };
+        // int[] nums = { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
+        int[] nums = { 1, 1, 2 };
+        RemoveDuplicates(nums);
+        Console.ReadLine();
+    }
+
+    static int RemoveDuplicates(int[] nums)
+    {
+        var uniqueItemPos = 0;
+        var nextUniqueItemPos = 0;
+        while (nextUniqueItemPos < nums.Length)
+        {
+            if (nums[uniqueItemPos] != nums[nextUniqueItemPos])
+            {
+                nums[++uniqueItemPos] = nums[nextUniqueItemPos];
+            }
+            nextUniqueItemPos++;
+        }
+
+        return uniqueItemPos + 1;
+    }
+}
+
+// Input: nums = [1,1,2]
+// Output: 2, nums = [1, 2, _]
+// Explanation: Your function should return k = 2, with the first two elements of nums being 1 and 2 respectively.
+// It does not matter what you leave beyond the returned k (hence they are underscores).
+
+// Test case 2
+// [0,0,1,1,1,2,2,3,3,4]
+
+// Test case 3
+// [1,2,3]
---
+++ leetcode/80RemoveDuplicatesFromSortedArrayII/Program.cs
@@ -0,0 +1,46 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] nums = { 1, 1, 1, 2, 2, 3 };
+        // int[] nums = { 1,2,2 };
+        RemoveDuplicates(nums);
+        Console.ReadLine();
+    }
+
+    static int RemoveDuplicates(int[] nums)
+    {
+        var uniqueItemPos = 0;
+        var nextUniqueItemPos = 0;
+        var maxUniqueAppearsCount = 2;
+        var uniqueAppearsCounter = 0;
+        while (nextUniqueItemPos < nums.Length)
+        {
+            if (nums[uniqueItemPos] != nums[nextUniqueItemPos])
+            {
+                nums[++uniqueItemPos] = nums[nextUniqueItemPos];
+                uniqueAppearsCounter = 0;
+            }
+            else if (uniqueAppearsCounter < maxUniqueAppearsCount)
+            {
+                uniqueAppearsCounter++;
+            }
+            else if (uniqueAppearsCounter == maxUniqueAppearsCount)
+            {
+                uniqueAppearsCounter = 0;
+                uniqueItemPos = nextUniqueItemPos;
+
+            }
+            nextUniqueItemPos++;
+        }
+
+        return nextUniqueItemPos;
+    }
+}
+
+//Example 1:
+
+//Input: nums = [1, 1, 1, 2, 2, 3]
+//Output: 5, nums = [1, 1, 2, 2, 3, _]
+//Explanation: Your function should return k = 5, with the first five elements of nums being 1, 1, 2, 2 and 3 respectively.
+//It does not matter what you leave beyond the returned k (hence they are underscores).
