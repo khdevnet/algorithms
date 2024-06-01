f164a67fcc77bf83a35660eeadc24686abb6b6ff
Anton Shcherbyna <khdevnet@gmail.com>
2024-06-01 15:20:11 +0000

    v2

--- leetcode/14LongestCommonPrefix/Program.cs
+++ leetcode/14LongestCommonPrefix/Program.cs
@@ -38,10 +38,26 @@
             else
             {
                 count = 0;
-                prefix = longestWord.Substring(0, longestWordLength - (i+1));
+                prefix = longestWord.Substring(0, longestWordLength - (i + 1));
             }
         }
 
         return "";
+    }
+
+    private static string LongestCommonPrefixV2(string[] strs)
+    {
+        if (strs.Length == 0)
+            return "";
+        string prefix = strs[0];
+        for (int i = 1; i < strs.Length; i++)
+            while (strs[i].IndexOf(prefix) != 0)
+            {
+                prefix = prefix.Substring(0, prefix.Length - 1);
+                if (prefix == "")
+                    return "";
+            }
+
+        return prefix;
     }
 }
