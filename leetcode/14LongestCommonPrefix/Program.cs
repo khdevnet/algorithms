53b9015cf30d0a1c4c0638352fe30b4f920a46a2
Anton Shcherbyna <khdevnet@gmail.com>
2024-06-01 14:59:48 +0000

    add my solution

--- leetcode/14LongestCommonPrefix/Program.cs
+++ leetcode/14LongestCommonPrefix/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // var strs = new string[] { "flower", "flow", "flight" };
+        var strs = new string[] { "ab", "a" };
+        var prefix = LongestCommonPrefix(strs);
+        Console.ReadLine();
+    }
+
+    private static string LongestCommonPrefix(string[] strs)
+    {
+        int longestWordLength = 0;
+        string longestWord = "";
+        foreach (var word in strs)
+        {
+            if (word.Length > longestWordLength)
+            {
+                longestWordLength = word.Length;
+                longestWord = word;
+            }
+        }
+        var prefix = longestWord;
+        for (int i = 0; i < longestWordLength; i++)
+        {
+            int count = 0;
+            for (int j = 0; j < strs.Length; j++)
+            {
+                if (strs[j].StartsWith(prefix))
+                {
+                    count++;
+                }
+            }
+            if (strs.Length == count)
+            {
+                return prefix;
+            }
+            else
+            {
+                count = 0;
+                prefix = longestWord.Substring(0, longestWordLength - (i+1));
+            }
+        }
+
+        return "";
+    }
+}
