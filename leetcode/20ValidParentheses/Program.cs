237af6de986d19c82bcb3dea9478816fc539c629
Anton Shcherbyna <khdevnet@gmail.com>
2024-06-02 10:31:35 +0000

    ValidParentheses

---
+++ leetcode/20ValidParentheses/Program.cs
@@ -0,0 +1,41 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // var s = "()[]{}";
+        var s = "){";
+        var output = IsValid(s);
+        Console.ReadLine();
+    }
+
+    private static readonly Dictionary<char, char> openClosedBracketsMap
+    = new()
+    {
+        {'(',')'},
+        {'[',']'},
+        {'{','}'}
+    };
+
+
+    private static bool IsValid(string s)
+    {
+        if (s.Length == 0) return true;
+        if (s.Length % 2 != 0) return false;
+
+        var nextClosedBrackets = new Stack<char>();
+        foreach (var bracket in s)
+        {
+            if (openClosedBracketsMap.ContainsKey(bracket))
+            {
+                nextClosedBrackets.Push(openClosedBracketsMap[bracket]);
+            }
+            else
+            {
+                if(nextClosedBrackets.Count == 0) return false;
+                var closedBracket = nextClosedBrackets.Pop();
+                if (closedBracket != bracket) return false;
+            }
+        }
+        return nextClosedBrackets.Count == 0;
+    }
+}
