bae92b8bf21df1a74ee99be4e703662398e6d86f
Anton Shcherbyna <khdevnet@gmail.com>
2024-06-03 20:40:50 +0000

    sort linked list

--- leetcode/21MergeTwoSortedLists/Program.cs
+++ leetcode/21MergeTwoSortedLists/Program.cs
@@ -18,27 +18,53 @@
 
         var list1 = new ListNode(1, new ListNode(2, new ListNode(4)));
         var list2 = new ListNode(1, new ListNode(3, new ListNode(4)));
-        var merged = MergeTwoLists(list1, list2);
+        var merged = MergeTwoLists2(list1, list2);
         Console.ReadLine();
+    }
+
+    private static ListNode MergeTwoLists2(ListNode list1, ListNode list2)
+    {
+        ListNode head = new ListNode();
+        ListNode tail = head;
+        while (list1 != null && list2 != null)
+        {
+            if (list1.val < list2.val)
+            {
+                tail.next = list1;
+                list1 = list1.next;
+            }
+            else
+            {
+                tail.next = list2;
+                list2 = list2.next;
+            }
+            tail = tail.next;
+        }
+
+        tail.next = list1 == null ? list2 : list1;
+
+        return head.next;
     }
 
     private static ListNode MergeTwoLists(ListNode list1, ListNode list2)
     {
-        if (list1.next == null && list2.next != null) return list2;
-        if (list1.next != null && list2.next == null) return list1;
-        var merged = new ListNode();
-        while (list1.next != null && list2.next != null)
-        {
-            if (list1.val <= list2.val)
-            {
-                merged.val = list1.val; 
-                merged.next = new ListNode(list2.val);
-                merged = merged.next;
-                list1 = list1.next;
-                list2 = list2.next;
-            }
+        if (list1 == null) return list2;
+        if (list2 == null) return list1;
 
+        ListNode mergedHead;
+        if (list1.val < list2.val)
+        {
+            mergedHead = list1;
+            list1 = list1.next;
         }
-        return list1;
+        else
+        {
+            mergedHead = list2;
+            list2 = list2.next;
+        }
+
+        mergedHead.next = MergeTwoLists(list1, list2);
+
+        return mergedHead;
     }
 }
