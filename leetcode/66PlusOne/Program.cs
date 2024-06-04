5841f1e094033094eee281d43f7d8f26aaaf9b18
Anton Shcherbyna <khdevnet@gmail.com>
2024-06-04 21:04:29 +0000

    plus

--- leetcode/66PlusOne/Program.cs
+++ leetcode/66PlusOne/Program.cs
@@ -3,29 +3,41 @@
     private static void Main(string[] args)
     {
         // var digits = new int[] { 1, 2, 3 };
-        var digits = new int[] { 4, 3, 9, 9 };
+        var digits = new int[] { 1, 9, 9 };
         var r = PlusOne(digits);
         Console.ReadLine();
     }
 
     private static int[] PlusOne(int[] digits)
     {
-        var newDigits = new int[10];
+        if (digits.Length == 0) return digits;
         var add = 1;
         for (int i = digits.Length - 1; i >= 0; i--)
         {
             var lastD = digits[i] + add;
             if (lastD == 10)
             {
-                newDigits[i] = 0;
+                digits[i] = 0;
                 add = 1;
             }
             else
             {
                 add = 0;
-                newDigits[i] = lastD;
+                digits[i] = lastD;
             }
         }
-        return newDigits;
+
+        if (add == 1)
+        {
+            var newDigits = new int[digits.Length + 1];
+            newDigits[0] = 1;
+            for (int i = 0; i < digits.Length; i++)
+            {
+                newDigits[i + 1] = digits[i];
+            }
+            return newDigits;
+        }
+
+        return digits;
     }
 }
