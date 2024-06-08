2f5caff59de2dd3466955b6ebd478383d11f2fcf
Anton Shcherbyna <khdevnet@gmail.com>
2024-06-08 11:52:56 +0000

    69sqrt sol

---
+++ leetcode/69Sqrt_x_/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var r = MySqrt(3);
+        Console.ReadLine();
+    }
+
+    // calculate using formula, sqrt x = e^1/2*logx
+    // e = Math.Exp(0.5*Math.log(x));
+    private static int MySqrt(int x)
+    {
+        if (x < 2) return x;
+        var left = (long)Math.Exp(0.5 * Math.Log(x));
+        var right = left + 1;
+        return (int)(right * right > x ? left : right);
+    }
+}
