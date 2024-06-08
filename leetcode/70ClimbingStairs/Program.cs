9dbfe0af095eac682ffcee3cb90827b64ae00f6d
Anton Shcherbyna <khdevnet@gmail.com>
2024-06-08 17:19:08 +0000

    Dynamic

--- leetcode/70ClimbingStairs/Program.cs
+++ leetcode/70ClimbingStairs/Program.cs
@@ -3,27 +3,44 @@
     private static void Main(string[] args)
     {
         var c = ClimbStairs(3);
+        var c2 = Climb_Stairs_BottomUp(3);
         Console.ReadLine();
     }
 
     private static int ClimbStairs(int n)
     {
-        return Climb_Stairs(0, n);
+        var mem = new Dictionary<int, int>();
+        return Climb_Stairs(0, n, mem);
     }
 
-    private static int Climb_Stairs(int i, int n)
+    // Complexity 2^n
+    // using memoize O(n)
+    private static int Climb_Stairs(int stair, int lastStair, Dictionary<int, int> mem)
     {
-        if (i > n)
-        {
-            return 0;
-        }
+        if (mem.ContainsKey(stair)) return mem[stair];
+        if (stair > lastStair) return 0;
+        if (stair == lastStair) return 1;
 
-        if (i == n)
-        {
-            return 1;
-        }
+        var count = Climb_Stairs(stair + 1, lastStair, mem) + Climb_Stairs(stair + 2, lastStair, mem);
+        mem[stair] = count;
+        return count;
+    }
 
-        return Climb_Stairs(i + 1, n) + Climb_Stairs(i + 2, n);
+    // dynamic programming, bottom up approach
+    // ith variations= (ith-1)+(ith-2)
+    // n=3  3 = (3-1)+ (3-2)
+    private static int Climb_Stairs_BottomUp(int n)
+    {
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+        int[] count = new int[n + 1];
+        count[1] = 1;
+        count[2] = 2;
+        for (int i = 3; i <= n; i++)
+        {
+            count[i] = (i - 1) + (i - 2);
+        }
+        return count[n];
     }
 
     // Example 1:
