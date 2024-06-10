96c6b5c0fdda53b983d7743ecfb751c435a59198
Anton Shcherbyna <khdevnet@gmail.com>
2024-06-10 21:04:36 +0000

    reverse string

---
+++ leetcode/344ReverseString/Program.cs
@@ -0,0 +1,18 @@
+﻿var l = new char[] { 'h', 'e', 'l', 'l', 'o' };
+var l2 = new char[] { 'H', 'a', 'n', 'n', 'a', 'h' };
+ReverseString(l2);
+Console.ReadLine();
+
+static void ReverseString(char[] s)
+{
+    R(s, 0, s.Length - 1);
+}
+
+static void R(char[] s, int i, int j)
+{
+    if (i >= j) return;
+    var b = s[i];
+    s[i] = s[j];
+    s[j] = b;
+    R(s, i + 1, j - 1);
+}
