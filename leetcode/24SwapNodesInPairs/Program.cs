641e056d3eabf888c20d19741c70e503d4863beb
Anton Shcherbyna <khdevnet@gmail.com>
2024-06-10 21:43:14 +0000

    lom

---
+++ leetcode/24SwapNodesInPairs/Program.cs
@@ -0,0 +1,29 @@
+﻿
+var head = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4))));
+var r = SwapPairs(head);
+
+Console.ReadLine();
+
+ListNode SwapPairs(ListNode head)
+{
+    if (head.next == null) return head;
+    var tail = head.next.next; // 3
+    var b = head.next; // 2
+    head.next = head; // 1
+    head = b; //2
+
+    return SwapPairs(tail);
+}
+
+
+class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
+
