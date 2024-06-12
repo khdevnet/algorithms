bcdcf683cb95d88bf006668518144bdf6d811376
Anton Shcherbyna <khdevnet@gmail.com>
2024-06-12 20:42:20 +0000

    leetcode

--- leetcode/24SwapNodesInPairs/Program.cs
+++ leetcode/24SwapNodesInPairs/Program.cs
@@ -17,6 +17,24 @@ ListNode SwapPairs(ListNode head)
     return newHead;
 }
 
+ListNode SwapPairsLeetCode(ListNode head)
+{
+    // If the list has no node or has only one node left.
+    if ((head == null) || (head.next == null))
+    {
+        return head;
+    }
+
+    // Nodes to be swapped
+    ListNode firstNode = head;
+    ListNode secondNode = head.next;
+    // Swapping
+    firstNode.next = SwapPairs(secondNode.next);
+    secondNode.next = firstNode;
+    // Now the head is the second node
+    return secondNode;
+}
+
 // st1  2 3
 // st2 3 4 new ListNode(head.next.val, null)
 // st3 return null;
