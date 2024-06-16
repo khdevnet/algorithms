2c7c52d8493c1bbce81aa4b45b599f40625d2a44
Anton Shcherbyna <khdevnet@gmail.com>
2024-06-16 09:27:45 +0000

    add pow with stack overflow

---
+++ leetcode/50Pow_X_n/Program.cs
@@ -0,0 +1,14 @@
+﻿
+var r = MyPow(2, -2);
+Console.ReadLine();
+
+double MyPow(double x, int n)
+{
+    return n > 0 ? Pow(x, n) : 1 / Pow(x, (n * -1));
+}
+
+double Pow(double x, int n)
+{
+    if (n == 0) return 1;
+    return x * Pow(x, n - 1);
+}
