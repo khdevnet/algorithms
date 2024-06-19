88b7c5ace6e152f8adb65c3b3f906fc77d03166a
Anton Shcherbyna <khdevnet@gmail.com>
2024-06-19 14:49:28 +0000

    moving average

---
+++ leetcode/346MovingAverageFromDataStream/Program.cs
@@ -0,0 +1,28 @@
+﻿MovingAverage obj = new MovingAverage(3);
+double param_1 = obj.Next(2);
+Console.ReadLine();
+
+public class MovingAverage
+{
+    private double sum;
+    private int size;
+    private readonly Queue<int> queue = new Queue<int>();
+    public MovingAverage(int size)
+    {
+        this.size = size;
+    }
+
+    public double Next(int val)
+    {
+        int first = queue.Count == size ? queue.Dequeue() : 0;
+        sum = sum - first + val;
+        queue.Enqueue(val);
+        return sum / queue.Count;
+    }
+}
+
+/**
+ * Your MovingAverage object will be instantiated and called as such:
+ * MovingAverage obj = new MovingAverage(size);
+ * double param_1 = obj.Next(val);
+ */
